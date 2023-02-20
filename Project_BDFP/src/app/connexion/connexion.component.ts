0fb41d58fcfb43b4dfea223cd8a551f7b6fcf28c
VOVC Dan-Gabriel <vovc.dan@gmail.com>
2023-02-20 10:12:39 +0100

    blabla

--- Project_BDFP/src/app/connexion/connexion.component.ts
+++ Project_BDFP/src/app/connexion/connexion.component.ts
@@ -38,8 +38,6 @@ export class ConnexionComponent implements OnInit {
       var sessionObject = JSON.parse(this.token.getSession() || '{}');
       var username = sessionObject.username;
       var userID = sessionObject.userID;
-      console.log(username);
-      console.log(userID);
       this.utilService.connect();
       this.utilService.setUserName(username);
       this.utilService.setUserId(userID);7
