9eb54cc88f75a0a1cac3807b58d7c25e3fb6bccf
LouisLangellier <louis.langellier@gmail.com>
2023-03-09 16:29:44 +0100

    new affichage films

--- Project_BDFP/src/app/affichage-films/affichage-films.component.ts
+++ Project_BDFP/src/app/affichage-films/affichage-films.component.ts
@@ -31,122 +31,53 @@ export class AffichageFilmsComponent implements OnInit {
     private router: Router,
     private utilService: UtilsService,
     private rechercheService: RechercheService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.init()
-    // this.filmService.getListsTitles();
-    // this.filmService
-    //   .getFilmsByUid(this.utilService.getUserId())
-    //   .subscribe((allfilms) => {
-    //     this.films = allfilms[0].movies;
-    //     this.utilService.setListOfFilms(this.films);
-    //     for (let i = 0; i < this.films.length; i++) {
-    //       this.api.getMovieTMDBByIMDBID(this.films[i].omdbID).subscribe((res: any) => {
-    //         console.log(res)
-    //         let title = res['movie_results'][0]?.title;
-    //         let release_date = res['movie_results'][0]?.release_date;
-    //         let values = {
-    //           IMDBID: this.films[i].omdbID,
-    //           TMDBID: res['movie_results'][0].id,
-    //           Title: title,
-    //           Release_Date: release_date,
-    //         };
-    //         let poster = this.filmService.getPoster(this.films[i].omdbID)
-    //         this.movies.set(poster, values)
-    //       })
-
-
-
-    // this.api
-    //   .getMovieTMDBByIMDBID(this.films[i].omdbID)
-    //   .subscribe((filmAPI: any) => {
-    //     let imdb_id_local = this.films[i].omdbID;
-    //     if (filmAPI['movie_results'][0]) {
-    //       let id = filmAPI['movie_results'][0].id;
-    //       let poster_path = filmAPI['movie_results'][0].poster_path;
-    //       let poster: String;
-    //       if (poster_path !== null) {
-    //         poster = 'https://image.tmdb.org/t/p/w300/' + poster_path;
-    //       } else {
-    //         poster = '../../assets/no-poster.png';
-    //       }
-    //       let title = filmAPI['movie_results'][0].title;
-    //       let release_date = filmAPI['movie_results'][0].release_date;
-    //       let values = {
-    //         IMDBID: imdb_id_local,
-    //         TMDBID: id,
-    //         Poster: poster,
-    //         Title: title,
-    //         Release_Date: release_date,
-    //       };
-    //       this.movies.set(filmAPI, values);
-    //     } else {
-    //       this.api
-    //         .getMovieById(this.films[i].omdbID)
-    //         .subscribe((filmAPI: any) => {
-    //           let id = filmAPI.imdbID;
-    //           let poster: String = filmAPI.Poster;
-    //           let title = filmAPI.Title;
-    //           let release_date = filmAPI.Year;
-    //           if (poster != 'N/A') {
-    //             let values = {
-    //               IMDBID: imdb_id_local,
-    //               TMDBID: '',
-    //               Poster: poster,
-    //               Title: title,
-    //               Release_Date: release_date,
-    //             };
-    //             this.movies.set(filmAPI, values);
-    //           } else {
-    //             this.noPoster = true;
-    //             poster = '../../assets/no-poster.png';
-    //             let values = {
-    //               IMDBID: imdb_id_local,
-    //               TMDBID: '',
-    //               Poster: poster,
-    //               Title: title,
-    //               Release_Date: release_date,
-    //             };
-    //             this.movies.set(filmAPI, values);
-    //           }
-    //         });
-    //     }
-    //   });
-    //   }
-    // });
+    this.init();
   }
 
   async init() {
-    let results = await this.filmService.getFilmsByUidAsync()
-    let allMovies = results[0].movies
+    let results = await this.filmService.getFilmsByUidAsync();
+    let allMovies = results[0].movies;
 
     for (let i = 0; i < allMovies.length; i++) {
-      let data = await this.api.getMovieTMDBByIMDBIDAsync(allMovies[i].omdbID);
-      let data_jsoned = await data.json()
-      let tmdbMovie = await this.api.getMovieTMDbIdAsync(data_jsoned.movie_results[0]?.id)
-      let tmdbMovie_jsoned = await tmdbMovie.json()
-      if (tmdbMovie_jsoned.poster_path !== null) {
-        let values = {
-          title: tmdbMovie_jsoned.title,
-          release_date: tmdbMovie_jsoned.release_date,
-          poster: 'https://image.tmdb.org/t/p/w300/' + tmdbMovie_jsoned.poster_path,
-          tmdbid: tmdbMovie_jsoned.id
+      let poster = '';
+      let tmdbMovie = await this.api.getMovieTMDBByIMDBIDAsync(allMovies[i].omdbID);
+      let tmdbMovie_jsoned = await tmdbMovie.json();
+      console.log(tmdbMovie_jsoned.movie_results[0]);
+      if(tmdbMovie_jsoned.movie_results[0]){
+        if(tmdbMovie_jsoned.movie_results[0].poster_path !== null){
+          poster = tmdbMovie_jsoned.movie_results[0].poster_path
+        } else {
+          poster = '../../assets/no-poster.png';
         }
-        this.movies.set(tmdbMovie_jsoned.imdb_id, values)
+        let values = {
+          title: tmdbMovie_jsoned.movie_results[0].title,
+          release_date: tmdbMovie_jsoned.movie_results[0].release_date,
+          poster: 'https://image.tmdb.org/t/p/w300' + poster,
+          tmdbid: tmdbMovie_jsoned.movie_results[0].id,
+        };
+        this.movies.set(allMovies[i].omdbID, values);
       } else {
-        let poster = await this.filmService.getPoster(allMovies[i].omdbID)
-        let values = {
-          title: tmdbMovie_jsoned.title,
-          release_date: tmdbMovie_jsoned.release_date,
-          poster: poster,
-          tmdbid: tmdbMovie_jsoned.id
+        let omdbMovie = await this.api.getMovieByIdAsync(allMovies[i].omdbID)
+        let omdbMovie_jsoned = await omdbMovie.json()
+        console.log(omdbMovie_jsoned)
+        if(omdbMovie_jsoned.Poster != 'N/A'){
+          poster = omdbMovie_jsoned.Poster
+        } else {
+          poster = '../../assets/no-poster.png';
         }
-        this.movies.set(allMovies[i].omdbID, values)
+        let values = {
+          title: omdbMovie_jsoned.Title,
+          release_date: omdbMovie_jsoned.Released,
+          poster: poster,
+          tmdbid: "",
+        };
+        this.movies.set(allMovies[i].omdbID, values);
       }
     }
 
     console.log(this.movies);
   }
-
 }
